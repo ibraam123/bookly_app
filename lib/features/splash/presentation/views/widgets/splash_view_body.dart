1ca0351bc4052bdec133400fc63c3e51a9e5d577
Ibraam Magdy <112469519+ibraam123@users.noreply.github.com>
2025-08-05 14:26:08 +0300

    adding sliding animation to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,11 +1,66 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _initSlidingAnimation() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(_animationController);
+    _animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset(Assets.logo));
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(Assets.logo),
+        const SizedBox(height: 10),
+        AnimatedBuilder(
+          animation: _slidingAnimation,
+          builder: (context, child) => Transform.translate(
+            offset: _slidingAnimation.value,
+            child: SlideTransition(
+              position: _slidingAnimation,
+              child: Text(
+                "Read Books For Free",
+                style: Theme.of(context).textTheme.titleMedium,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
+
