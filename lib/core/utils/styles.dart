0cd49f2f4676094bdde47565c0f6945da2f64a38
Ibraam Magdy <112469519+ibraam123@users.noreply.github.com>
2025-08-05 19:08:09 +0300

    finish styles text

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,10 +1,26 @@
-
-
+import "package:bookly_app/constants.dart";
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
 
 abstract class Styles {
-  static final TextStyle titleMedium = TextStyle(
-    fontSize: 20,
+  static final TextStyle textStyle18 = TextStyle(
+    fontSize: 18.sp,
     fontWeight: FontWeight.w600,
+  );
+  static final TextStyle textStyle30 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+  static final TextStyle textStyle20 = TextStyle(
+    fontSize: 20.sp,
+    fontFamily: kGtSectraFine,
+  );
+
+  static final TextStyle textStyle14 = TextStyle(fontSize: 14.sp);
+
+  static final TextStyle textStyle16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
   );
 }
