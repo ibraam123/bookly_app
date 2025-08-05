01523dcf572ed1650729e9b2c9f50d1c40359c45
Ibraam Magdy <112469519+ibraam123@users.noreply.github.com>
2025-08-05 13:44:24 +0300

    splash view done

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,6 @@
+
+class Assets {
+  static const String images = 'assets/images';
+  static const String fonts = 'assets/fonts';
+  static const String logo = '$images/Logo.png';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -12,9 +13,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      home: const SplashView(),
     );
   }
 }
